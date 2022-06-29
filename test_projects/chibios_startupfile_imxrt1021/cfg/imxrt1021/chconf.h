b5f630f7abc6f09fd3632c81c869be082e1a2344
Edward <Edward.thomson9@gmail.com>
2022-06-29 10:52:46 +1000

    Tiny config change for future compilations

--- test_projects/chibios_startupfile_imxrt1021/cfg/imxrt1021/chconf.h
+++ test_projects/chibios_startupfile_imxrt1021/cfg/imxrt1021/chconf.h
@@ -142,14 +142,14 @@
 /**
  * @brief   Kernel hardening level.
  * @details This option is the level of functional-safety checks enabled
- *          in the kerkel. The meaning is:
+ *          in the kernel. The meaning is:
  *          - 0: No checks, maximum performance.
  *          - 1: Reasonable checks.
  *          - 2: All checks.
  *          .
  */
 #if !defined(CH_CFG_HARDENING_LEVEL)
-#define CH_CFG_HARDENING_LEVEL              0
+#define CH_CFG_HARDENING_LEVEL              2
 #endif
 
 /** @} */
